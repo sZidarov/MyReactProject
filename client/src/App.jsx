f989b375867258911bb5852ade92f82ccde796a1
sZidarov <s.zidarov@gmail.com>
2023-12-09 02:58:03 +0200

    add error boundary

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,35 +20,38 @@ import Logout from "./components/Logout";
 import AuthGuard from "./guards/authGuard";
 import AdminGuard from "./guards/adminGuard";
 import AllReservations from "./components/allReservationsPage";
+import ErrorBoundary from "./components/ErrorBounary";
 
 function App() {
     return (
         <>
-            <AuthProvider>
-                <TopBar></TopBar>
-                <Navbar></Navbar>
-                <Routes fallback={<h1>Loading. . .</h1>}>
-                    <Route path={Path.Home} element={<Home/>} />
-                    <Route path={Path.Login} element={<Login/>} />
-                    <Route path={Path.Register} element={<Register/>} />
-                    <Route path={Path.About} element={<About/>} />
-                    <Route path={Path.Rooms} element={<RoomsCatalog/>} />
-                    <Route path={Path.Contacts} element={<Contacts/>} />
-                    <Route path={Path.Details} element={<Details/>} />
-                    <Route element={<AuthGuard/>}>
-                        <Route path={Path.Booking} element={<Bookingform/>} />
-                        <Route path={Path.Logout} element={<Logout />} />
-                        <Route path={Path.MyReservations} element={<MyReservations/>}/>
-                        <Route path={Path.AllReservations} element={<AllReservations/>}/>
-                    </Route>
-                    <Route element={<AdminGuard/>}>
-                        <Route path={Path.CreateRoom} element={<CreateRoom/>} />
-                        <Route path={Path.EditRoom} element={<EditRoom/>} />
-                    </Route>
-                </Routes>
-                
-                <Footer> </Footer>
-            </AuthProvider>
+            <ErrorBoundary>
+                <AuthProvider>
+                    <TopBar></TopBar>
+                    <Navbar></Navbar>
+                    <Routes fallback={<h1>Loading. . .</h1>}>
+                        <Route path={Path.Home} element={<Home/>} />
+                        <Route path={Path.Login} element={<Login/>} />
+                        <Route path={Path.Register} element={<Register/>} />
+                        <Route path={Path.About} element={<About/>} />
+                        <Route path={Path.Rooms} element={<RoomsCatalog/>} />
+                        <Route path={Path.Contacts} element={<Contacts/>} />
+                        <Route path={Path.Details} element={<Details/>} />
+                        <Route element={<AuthGuard/>}>
+                            <Route path={Path.Booking} element={<Bookingform/>} />
+                            <Route path={Path.Logout} element={<Logout />} />
+                            <Route path={Path.MyReservations} element={<MyReservations/>}/>
+                            <Route path={Path.AllReservations} element={<AllReservations/>}/>
+                        </Route>
+                        <Route element={<AdminGuard/>}>
+                            <Route path={Path.CreateRoom} element={<CreateRoom/>} />
+                            <Route path={Path.EditRoom} element={<EditRoom/>} />
+                        </Route>
+                    </Routes>
+                    
+                    <Footer> </Footer>
+                </AuthProvider>
+            </ErrorBoundary>
         </>
     );
 }
